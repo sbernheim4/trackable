218dae7a2acfb5dd9ef9bded4f27730020e35842
Samuel Bernheim <sambernheim@gmail.com>
2023-08-26 01:00:29 -0400

    docs: add comments

--- src/index.test.ts
+++ src/index.test.ts
@@ -6,8 +6,11 @@ describe("Trackable", () => {
 
 	describe('track random numbers that are generated in application code function bodies', () => {
 
-		// A function that returns a trackable instance where the random number
-		// is stored in the analytics event payload.
+		// First we write some functinos that mimic application code where there
+		// is some business logic and invocations to other functions to get
+		// data.
+		//
+		// Here's the 1st
 		const addThreeAndRandom = (val: number) => {
 			const random = Math.random();
 
@@ -20,8 +23,7 @@ describe("Trackable", () => {
 			);
 		};
 
-		// Another function that returns a trackable instance where the random
-		// number is stored in the analytics event payload.
+		// Here's the 2nd
 		const multiplyBy5AndRandom = (val: number) => {
 			const random = Math.random();
 
@@ -34,45 +36,65 @@ describe("Trackable", () => {
 			);
 		};
 
-		// A function without anything specific to be tracked.
+		// And here's the 3rd - this one doesn't have any specific data that we
+		// want to track - its pure business logic.
 		const addTwo = (x: number) => x + 2;
 
 		it("there are 3 events when there are 3 functions invoked for a trackable", () => {
 
 			const trackingFunction = vi.fn();
 
-			// Now we can easily interact with our Trackable instance.
-			const res = addThreeAndRandom(5)
-				// flatmapping when the function provided returns a Trackable
-				// instance itself
-				.flatMap(multiplyBy5AndRandom)
-				// And regular mapping when the function provided does not
-				// return a trackable instance itself;
+			// We construct our initial instance which is a value whose function
+			// calls we want to fire analytics events for.
+			const valueToTrack = Trackable.of(5);
+
+			// Here we being using those instance methods just like we would for
+			// an Array:
+			const res = valueToTrack
+				.flatMap(addThreeAndRandom)
 				.map(addTwo)
+				.flatMap(multiplyBy5AndRandom)
 				.run(trackingFunction);
 
+			// Invoking `run` returns to us the underlying value (a number) and
+			// fires off all the analytics events so we expect our result to be
+			// in the following range given the math we've done:
 			expect(res).toBeGreaterThan(40);
-			expect(res).toBeLessThan(59);
+			expect(res).toBeLessThan(64);
 
+			// The trackingFunction should have been invoked once and given all
+			// the analytics events which includes the random numbers used at
+			// each function call.
 			expect(trackingFunction).toHaveBeenCalledTimes(1);
 
 			const events = trackingFunction.mock.calls[0][0] as AnalyticsEvent[]
-
 			const metadata = events.map(x => x.metadata);
 			const randomNumbersUsed = events.map(x => x['randomNumber'] ?? false).filter(Boolean);
-
 			const eventValues = metadata.map(entry => entry?.currentValue ?? false).filter(Boolean);
 			const callers = metadata.map(entry => entry?.caller ?? false).filter(Boolean);
 			const inputs = metadata.map(entry => entry?.input ?? false).filter(Boolean);
 
-			expect(events.length).toBe(3);
+			// And we expect 4 events (1 for the inital construction of the
+			// trackable and 3 from the calling functions).
+			expect(events.length).toBe(4);
+
 			expect(callers).toStrictEqual([
-				"multiplyBy5AndRandom",
+				'addThreeAndRandom',
 				"addTwo",
+				"multiplyBy5AndRandom",
 			]);
+
+			// There should be 2 randomNumbers in the analytics events from
+			// invoking `addThreeAndRandom` and `multiplyBy5AndRandom`.
 			expect(randomNumbersUsed.length).toBe(2);
-			expect(eventValues.length).toBe(3);
-			expect(inputs.length).toBe(2);
+
+			// We also track all the values at each map/flatmap invokation so we
+			// expect 4 of them.
+			expect(eventValues.length).toBe(4);
+
+			// And we expect there to be three `input` key/value pairs (one for
+			// each map/flatMap call).
+			expect(inputs.length).toBe(3);
 
 		});
 
